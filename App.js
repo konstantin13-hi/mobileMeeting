641e8f3183cac45f74f7dabd3a2d3f4f7f1efd48
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2024-03-24 15:45:35 +0100

    navigation

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 
-import { Text, View } from 'react-native';
+// import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
+// export default function App() {
+//   return (
+//   <View className="flex-1 items-center justify-center ">
+//       <Text className="text-red-500">Osdadsada!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+// In App.js in a new project
+
+
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
   return (
-  <View className="flex-1 items-center justify-center ">
-      <Text className="text-red-500">Osdadsada!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
