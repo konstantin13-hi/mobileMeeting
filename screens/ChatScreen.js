12f4f19aeb9feed161ee98b8f78d1939b8829f3c
konstantin13-hi <konstantinchizhevskiy@gmail.com>
2024-04-24 15:59:31 +0200

    added localization, but it will be changed

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,45 +1,79 @@
-import React, { Component,useState } from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native'; // Добавляем компонент Picker
-import * as Localization from 'expo-localization';
-import { I18n } from 'i18n-js';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Modal, FlatList } from 'react-native';
+import { I18nextProvider, useTranslation } from 'react-i18next';
+import i18next, { languageResources } from '../i18n';
+import languagesList from '../languagesList.json';
 import {Picker} from '@react-native-picker/picker';
 
-const translations = {
-  en: { welcome: 'Hello', name: 'Charlie' },
-  ja: { welcome: 'こんにちは', name: 'んに' }
-};
+const ChatScreen = () => {
+  const [visible, setVisible] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState('en'); // Выбранный язык
+  const { t } = useTranslation();
 
-const i18n = new I18n(translations)
-i18n.locale = Localization.locale;
-i18n.enableFallback = true;
-console.log(Localization.getLocales());
-
-function ChatScreen (props){
-  const [selectedLanguage, setSelectedLanguage] = useState();
+  const changeLng = (lng) => {
+    i18next.changeLanguage(lng);
+    setVisible(false);
+  };
 
   return (
-    <View style={styles.container}>
-    <Text style={styles.text}>
-      {i18n.t('welcome')} {i18n.t('name')}
-    </Text>
-    <Text>Current locale: {i18n.locale}</Text>
-    <Text>Device locale: {Localization.getLocales()[0].languageCode}</Text>
-  </View>
+    <SafeAreaView style={styles.container}>
+      <Modal visible={visible} onRequestClose={() => setVisible(false)}>
+    
+      </Modal>
+
+      <Text style={styles.text}>{t('welcome')}</Text>
+      {/* Пикер для выбора языка */}
+      <Picker
+        selectedValue={selectedLanguage}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => {
+          setSelectedLanguage(itemValue);
+          changeLng(itemValue); // Вызываем функцию изменения языка при выборе значения в Пикере
+        }}>
+        {/* Опции выбора языка */}
+        {Object.keys(languageResources).map((lng) => (
+          <Picker.Item key={lng} label={languagesList[lng].nativeName} value={lng} />
+        ))}
+      </Picker>
+    </SafeAreaView>
   );
-  
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  text: {
-    fontSize: 20,
-    marginBottom: 16,
-  },
+  // button: {
+  //   backgroundColor: '#6258e8',
+  //   padding: 10,
+  //   borderRadius: 3,
+  //   marginTop: 10,
+  // },
+  // buttonText: {
+  //   color: 'white',
+  //   fontSize: 16,
+  // },
+  // text: {
+  //   marginBottom: 10,
+  //   fontSize: 18,
+  // },
+  // languagesList: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   padding: 10,
+  //   backgroundColor: '#6258e8',
+  // },
+  // languageButton: {
+  //   padding: 10,
+  //   borderBottomColor: '#dddddd',
+  //   borderBottomWidth: 1,
+  // },
+  // lngName: {
+  //   fontSize: 16,
+  //   color: 'white',
+  // },
 });
 
 export default ChatScreen;
