d45d2a6301f799119e4381c86813814c1f348dd5
konstantin13-hi <konstantinchizhevskiy@gmail.com>
2024-05-13 22:48:54 +0200

    added functionality in modal(update profile,but it ll be changed)

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Text, View ,TextInput, TouchableOpacity} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import useHookAuth from '../hooks/useAuth';
-import { useState } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import useAuth from "../hooks/useAuth";
 import { doc, setDoc } from "firebase/firestore";
 import { db, timestamp } from "../firebase";
-
+import { useNavigation } from "@react-navigation/native";
 
 const ModalScreen = () => {
-  const { user } = useHookAuth(); // Деструктурируем объект, чтобы получить свойство user
-  const[age,setAge] = useState();
-  const[image,setImage] = useState();
-  
+  const { user } = useAuth();
+  const navigation = useNavigation();
+  const [image, setImage] = useState("");
+  const [age, setAge] = useState("");
+  const [job, setJob] = useState("");
+
+  const incompleteForm = !image || !age || !job;
 
   const updateUserProfile = () => {
     setDoc(doc(db, "users", user.uid), {
       id: user.uid,
       displayName: user.displayName,
       photoURL: image,
+      job,
       age,
       timestamp,
     })
@@ -28,38 +37,60 @@ const ModalScreen = () => {
         Alert.alert("Error", err.message);
       });
   };
-  console.log(user);
 
   return (
-    <SafeAreaView>
-      
-        <Text>Welcome,{user.displayName} </Text> 
-        <Text>The profile pic</Text>
-        <TextInput placeholder='press picture' className="border"
-        keyboardType='url'
+    <View className="flex-1 items-center pt-1">
+    
+      <Text className="text-xl text-gray-500 p-2 font-bold">
+        Welcome {user.displayName}
+      </Text>
+
+      <Text className="text-center p-4 font-bold text-red-400">
+        Step 1: The Profile Pic
+      </Text>
+
+      <TextInput
+        placeholder="Enter a profile pic url"
+        className="text-center text-xl pb-2"
+        keyboardType="url"
         value={image}
-        onChange={(text)=>setImage(text)}>
-          
-        </TextInput>
-        <Text>Press yout age</Text>
-        <TextInput placeholder='press picture' className="border"
-        keyboardType='numeric'
+        onChangeText={setImage}
+      />
+      <Text className="text-center p-4 font-bold text-red-400">
+        Step 2: The Job
+      </Text>
+
+      <TextInput
+        placeholder="Enter your occupation"
+        className="text-center text-xl pb-2"
+        onChangeText={setJob}
+        value={job}
+      />
+      <Text className="text-center p-4 font-bold text-red-400">
+        Step 3: The Age
+      </Text>
+
+      <TextInput
+        placeholder="Enter your age"
+        className="text-center text-xl pb-2"
+        keyboardType="numeric"
         value={age}
-        onChange={(text)=>setAge(text)}>
-        </TextInput>
-        <TouchableOpacity onPress={updateUserProfile}>
-          <View className="border">
-            <Text>Update</Text>
-          </View>
+        onChangeText={setAge}
+        maxLength={2}
+      />
 
-        </TouchableOpacity>
-
-
-
+      <TouchableOpacity
+        disabled={incompleteForm}
+        className=
+          "w-64 p-3 rounded-xl absolute bottom-10 bg-red-400"
         
-        
-      
-    </SafeAreaView>
+        onPress={updateUserProfile}
+      >
+        <Text className="text-center text-white text-xl">
+          Update Profile
+        </Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
